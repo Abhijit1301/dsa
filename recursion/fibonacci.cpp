1c0634f185d5ca315bbe2a2e72d53fa1b9a782b5
Abhijeet <abhi.abhijit90@gmail.com>
2022-04-25 00:42:24 +0530

    added memorization

--- recursion/fibonacci.cpp
+++ recursion/fibonacci.cpp
@@ -1,20 +1,33 @@
 #include <iostream>
 
-long long getFibonacci(int n) {
+long long getFibonacci(int n, long long* memo) {
     if (0 == n) {
         return 1;
     }
     if (1 == n) {
         return 1;
     }
-    // This recursive method will take a very long time for n = 45 or more.
-    return getFibonacci(n - 1) + getFibonacci(n - 2);
+    if (n < 50) {
+        if (memo[n] != -1) {
+            return memo[n];
+        }
+        memo[n] = getFibonacci(n - 1, memo) + getFibonacci(n - 2, memo);
+        return memo[n];
+    }
+    // memorizing for faster calculation.
+    // will easily work till 85.
+
+    return getFibonacci(n - 1, memo) + getFibonacci(n - 2, memo);
 }
 
 int main(int argc, char** argv) {
+    long long memo[50];
+    for (int i = 0; i < 50; i++) {
+        memo[i] = -1;
+    }
     int input;
     std::cout << "Enter the number to get its fibonacci value: ";
     std::cin >> input;
     std::cout << std::endl;
-    std::cout << "Fibonacci of " << input << " is: " << getFibonacci(input) << std::endl;
+    std::cout << "Fibonacci of " << input << " is: " << getFibonacci(input, memo) << std::endl;
 }
